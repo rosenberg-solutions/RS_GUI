6f6476a230cfd2406cb65f4360dcf1f108982118
Olivia-Feldman <oyfeldman@gmail.com>
2020-12-23 12:16:52 -0600

    created base GUI

--- RSGUI/src/GUIDriver.java
+++ RSGUI/src/GUIDriver.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class GUIDriver {
+
+    public static void main(String[] args){
+
+        MyJPanel gui = new MyJPanel();
+        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
 }
+
--- RSGUI/src/MyJPanel.java
+++ RSGUI/src/MyJPanel.java
@@ -1,4 +1,36 @@
-public class MyJPanel {
-    //trying to commit a branch with token
+/**
+ * My JPanel Class will implement the GUI and allow user
+ * to input user information
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.plaf.FontUIResource;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyJPanel extends JFrame {
+
+    private JFrame f;
+    private JPanel p;
+
+
+    /**
+     * Constructor initializes panel
+     */
+    MyJPanel(){
+
+        setTitle("Rosenberg Solutions ");
+        setLayout(null);
+        setBounds(100,100, 800,800 );
+        p = new JPanel();
+        add(p);
+        p.setBounds(400,400,200,200);
+        setVisible(true);
+
+    };
+
 
 }
