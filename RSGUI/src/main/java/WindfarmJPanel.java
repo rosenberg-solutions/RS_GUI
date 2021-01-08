e0fd2714a2fe382a84a04249e555690448570270
Olivia-Feldman <oyfeldman@gmail.com>
2021-01-07 22:14:59 -0600

    WindFarm needs work on writing to JSON objects

--- RSGUI/src/main/java/WindfarmJPanel.java
+++ RSGUI/src/main/java/WindfarmJPanel.java
@@ -4,16 +4,19 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 public class WindfarmJPanel extends JFrame {
 
     static private JPanel p;
-    static private JButton add;
+    static private JButton add, save;
     static private JLabel numTurbine, turbineLabel, idLabel, xlabel,rmpLabel, pitchLabel, fileLabel, initIndDistLabel, initInductionLabel,nRotLabel, rot_trefftzLabel;
     private int countTurbine =0;
-    static private JSONObject windFarmObject, n_turbines, turbines, ID, X, rpm, pitch, outputFile, initIndDist, initInduction, nRot, rot_trefftz;
+    static private JSONObject windFarmObject,turbine, n_turbines, ID, X, rpm, pitch, outputFile, initIndDist, initInduction, nRot, rot_trefftz;
     static private JTable windTable;
     static private JScrollPane scrollTable;
+    //static private JSONArray turbine;
     JPanel getJPanel(){
 
 
@@ -28,6 +31,8 @@ public class WindfarmJPanel extends JFrame {
         add.setBounds(350,50,125,30);
         turbineLabel = new JLabel("Number of Turbines: " + Integer.toString(countTurbine));
         turbineLabel.setBounds(200,50,150,30);
+        save = new JButton( "save");
+        save.setBounds(500,650,100,30);
 
         //Creating a table for turbine input data
         DefaultTableModel model= new DefaultTableModel(); //this default will allow rows to be added
@@ -44,22 +49,40 @@ public class WindfarmJPanel extends JFrame {
         windTable = new JTable(model);
         scrollTable =new JScrollPane(windTable);
         scrollTable.setBounds(200,200,700,400);
+        windFarmObject = new JSONObject();
+        windFarmObject.put("n_turbines", countTurbine);
 
+
+
+        //adds rows to JTable each row represents a n_turbine
         add.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 countTurbine = countTurbine +1;
                 turbineLabel.setText("Number of Turbines: " + Integer.toString(countTurbine));
-                model.addRow(new Object[]{Integer.toString(countTurbine), "null","null","null ","null ","null","null "," null","null","null"});
+                model.addRow(new Object[]{Integer.toString(countTurbine), "1","2","3 ","0","0","0","0","0","0"});
+            }
+        });
+        //saving the data from the JTable into new JSON Objects.
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //need to access each element int the table and put into JSON array.
+              //  windFarmObject.put("turbine", model.getDataVector().elementAt(windTable.getSelectedRow()));
+              turbine  = new JSONObject();
+                turbine.put(("ID"),model.getValueAt(0,1));
+                turbine.put(("X"),model.getValueAt(0,2));
+
+                System.out.println(turbine);
             }
         });
 
-        windFarmObject = new JSONObject();
-        windFarmObject.put("n_turbines", countTurbine);
-
+        windFarmObject.put("turbine",turbine);
+        System.out.println(windFarmObject);
         p.add(add);
         p.add(turbineLabel);
         p.add(scrollTable);
+        p.add(save);
 
         return  p;
     }
